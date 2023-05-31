fc15bcaaac763cb7b7aa580c56a1da091b01849d
Harald McGerhard <hnipen@gmail.com>
2023-05-31 12:34:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My parents new UNhealthy dinner')
 
 streamlit.header('Breakfast menu')
-streamlit.text('🥣   mega 3 & Bleuberry')
+streamlit.text('🥣   Omega 3 & Blueberry')
 streamlit.text('🥗   Kale, Spinach & Rocket Smnoothie')
 streamlit.text('🐔   Hard-boiled Free-range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
